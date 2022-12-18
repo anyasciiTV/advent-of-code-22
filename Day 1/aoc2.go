cd1002000ac0da4df59842ef7abdbf58b9476e87
anyasciiTV <119780229+anyasciiTV@users.noreply.github.com>
2022-12-18 11:44:31 -0800

    Update aoc2.go

--- Day 1/aoc2.go
+++ Day 1/aoc2.go
@@ -10,15 +10,18 @@ import (
 func main() {
 
 	// Read the input file -- "input.txt"
-	input, _ := os.Open("/d/advent-of-code/Day2/part2/input.txt")
+	
+	input, _ := os.Open("input.txt")
 	defer input.Close()
 	sc := bufio.NewScanner(input)
 
 	// Search for the max sum of calories
+	
 	maxCalories := 0
 	currentCalories := 0
 
-	// Scan file
+	// For loop
+	
 	for sc.Scan() {
 		snack, err := strconv.Atoi(sc.Text())
 		currentCalories += snack
